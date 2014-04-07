1f7d05a0a4b88d22a735960dce6db07296dff0ff
Víctor Berga <vbergae@gmail.com>
2014-04-07 22:52:19 +0200

    Creates hello world! isles appp

--- web/main.php
+++ web/main.php
@@ -1,2 +1,12 @@
 <?php
-    echo 'Hello world!';
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app["debug"] = true;
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
