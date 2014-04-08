09e4b9e495a3c31106bca6a65587585eecb4cbe1
Víctor Berga <vbergae@gmail.com>
2014-04-08 21:00:03 +0200

    Creates base layout (IC-22)

--- web/main.php
+++ web/main.php
@@ -5,8 +5,18 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app["debug"] = true;
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/', function() use ($app) {
+   return $app['twig']->render('index.html.twig'); 
+});
+
 $app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+    return $app['twig']->render('hello.html.twig', array(
+        'name' => $app->escape($name))
+    );
 });
 
 $app->run();
