e1a98850b1f48615ab3154417a2f5df0a6c6ff77
Víctor Berga <vbergae@gmail.com>
2014-04-09 20:45:00 +0200

    Renames templates from extension .html to .html.twig

--- src/controllers.php
+++ src/controllers.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 //Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get('/', function() use ($app) {
-   return $app['twig']->render('index.html'); 
+   return $app['twig']->render('index.html.twig'); 
 });
 
 $app->get('/dashboard', function() use ($app) {
@@ -23,7 +23,7 @@ $app->get('/dashboard', function() use ($app) {
         $user[] = 'My Project '.($i + 1);
     }    
     
-    return $app['twig']->render('dashboard.html', array (
+    return $app['twig']->render('dashboard.html.twig', array (
        'starred_projects'   => $starred,
         'user_projects'     => $user
     ));
@@ -34,12 +34,12 @@ $app->error(function (\Exception $e, $code) use ($app) {
         return;
     }
 
-    // 404.html, or 40x.html, or 4xx.html, or error.html
+    // 404.html.twig, or 40x.html.twig, or 4xx.html.twig, or error.html.twig
     $templates = array(
-        'errors/'.$code.'.html',
-        'errors/'.substr($code, 0, 2).'x.html',
-        'errors/'.substr($code, 0, 1).'xx.html',
-        'errors/default.html',
+        'errors/'.$code.'.html.twig',
+        'errors/'.substr($code, 0, 2).'x.html.twig',
+        'errors/'.substr($code, 0, 1).'xx.html.twig',
+        'errors/default.html.twig',
     );
 
     return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
