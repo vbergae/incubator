92110d0d1ce96be7136a2150649abf4c29d98c34
Víctor Berga <victor.berga@glass.u-tad.com>
2014-04-09 15:56:42 +0200

    Improves folder structure

---
+++ src/controllers.php
@@ -0,0 +1,35 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+//Request::setTrustedProxies(array('127.0.0.1'));
+
+$app->get('/', function() use ($app) {
+   return $app['twig']->render('index.html'); 
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return $app['twig']->render('hello.html', array(
+        'name' => $app->escape($name))
+    );
+});
+
+$app->error(function (\Exception $e, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    // 404.html, or 40x.html, or 4xx.html, or error.html
+    $templates = array(
+        'errors/'.$code.'.html',
+        'errors/'.substr($code, 0, 2).'x.html',
+        'errors/'.substr($code, 0, 1).'xx.html',
+        'errors/default.html',
+    );
+
+    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
