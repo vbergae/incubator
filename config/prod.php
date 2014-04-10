51ea7671e55833feed135f79a2b4f1d10d70e876
Víctor Berga <vbergae@gmail.com>
2014-04-11 00:05:23 +0200

    Installs doctrine

--- config/prod.php
+++ config/prod.php
@@ -4,3 +4,27 @@
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options'    => array(
+    'driver'        => 'pdo_mysql',
+    'host'          => 'localhost',
+    'dbname'        => 'name',
+    'user'          => 'root',
+    'password'      => '',
+    'charset'       => 'utf8',
+    'driverOptions' => array(1002 => 'SET NAMES utf8',),
+  ),
+));
+
+$app->register(new Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider, array(
+    "orm.em.options" => array(
+         "mappings" => array(
+            array(
+               "type"      => "yml",
+               "namespace" => "Entity",
+               "path"      => realpath(__DIR__."/../config/doctrine"),
+              ),
+            ),
+         ),
+));
